bf71442ec8081b8df7c7d83985b6ed2e49d97f3c
ImpactSeo <impactseofr@gmail.com>
2016-04-01 15:00:19 +0200

    debug

--- api_callback.php
+++ api_callback.php
@@ -7,19 +7,35 @@ $rawData = json_decode($rawData);
 error_log('api_callback  - text id : ' . $rawData->id);
 error_log('api_callback  - result : ' . print_r($rawData, true));
 
+$result_dir = __DIR__ . '/../results/';
+
 if($rawData)
 {
-	# Save results in database or folder
-	$filename = __DIR__ . '/../results/' . $rawData->id . '.txt';
-	$saved = file_put_contents($filename, $rawData);
-	if($saved!==fase)
+	# Create results directory if missing
+	if(!is_dir($result_dir))
 	{
-		# Result saved
-		error_log('api_callback - ' . $filename . ' saved');
+		mkdir($result_dir, 0777);
+	}
+
+	# Test directory is writable
+	if(is_writable($result_dir))
+	{
+		# Save results in database or folder
+		$filename = $result_dir . $rawData->id . '.txt';
+		$saved = file_put_contents($filename, $rawData);
+		if($saved!==false)
+		{
+			# Result saved
+			error_log('api_callback - ' . $filename . ' saved');
+		}
+		else
+		{
+			error_log('api_callback - error saving ' . $filename . '. Check file/folder permissions.');	
+		}
 	}
 	else
 	{
-		error_log('api_callback - error saving ' . $filename . '. Check file/folder permissions.');	
+		error_log('api_callback - Make directory ' . $result_dir . ' writable !');
 	}
 }
 else
