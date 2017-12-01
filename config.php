1462e975be790fb03950b03a1ba9ed7c7e04988f
hugo <hugo@maugey.fr>
2017-12-01 13:55:08 +0100

    change API_ENDPOINT to app

--- config.php
+++ config.php
@@ -6,4 +6,4 @@ define('API_KEY', 'YOUR_API_KEY');
 define('API_CALLBACK', 'YOUR_API_CALLBACK');
 
 # Do not modify our API Endpoint
-define('API_ENDPOINT', 'https://www.killduplicate.com/api/public/');
+define('API_ENDPOINT', 'https://app.killduplicate.com/api/public/');
